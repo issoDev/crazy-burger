fce5cb86ae2db6b118cce235215c82b5bd332d2c
Issife <issife@hotmail.fr>
2025-08-03 00:56:12 +0200

    ✨ F01: LOGIN > Add title ann form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
 
 export function LoginPage() {
-  return <div>LoginPage</div>
-  
+  //state
+  const [inputValue, setInputValue] = useState("")
+
+  //comportements
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
+
+  const handleChange = (e) => { 
+    const value = e.target.value
+    setInputValue(value.trim())
+  }
+
+  //affichage (rendre)
+  return (
+    <div>
+      <h2>Bienvenue chez nous</h2>
+      <br />
+      <h3>Connectez-vous</h3>
+      <form 
+        action="submit"
+        onSubmit={handleSubmit}
+      >
+        <input 
+          type="text" 
+          placeholder="Entrez votre prénom..."
+          required 
+          value={inputValue}
+          onChange={handleChange}
+        />
+          <button>Accédez à votre espace</button>
+      </form>
+    </div>
+    
+  )
 }
