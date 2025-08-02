933b5da47f4c169708654c0e2ad6daf6d1bb597f
Issife <issife@hotmail.fr>
2025-08-03 00:35:07 +0200

    ✨ F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
-import { useState } from "react"
+import { LoginPage } from "./components/pages/LoginPage";
 
 function App() {
-  // state(état, données)
-  //un state provoque une réactualisation automatique de l'affichage
-  const [inputValue, setInputValue] = useState("");
-  
-
-  // comportements
-  const inputValueUpdated= (e) => {
-    const valueAfterChange = e.target.value;
-    setInputValue(valueAfterChange.trim())
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (inputValue) {
-      alert(`Bonjour ${inputValue}`)
-    }
-    setInputValue("")
-  }
-
-  // affichage(une fois le state modifié, l'actualisation de l'affichage se automatiquement)
-  return (
-    <div>
-      <h2>Bienvenue chez nous</h2>
-      <div>
-        <h3>Connectez-vous</h3>
-          <form action="submit" onSubmit={handleSubmit}>
-            <input 
-            type="text" 
-            placeholder="Entrez votre prénom..."
-            value={inputValue}
-            onChange={inputValueUpdated}
-            required
-          />
-          <button type="submit">Accédez à votre espace</button>
-        </form>
-      </div>
-      
-    </div>
-  ) 
+  return <LoginPage /> 
 }
-
 
 
 export default App
